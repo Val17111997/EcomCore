05f4788b0cb0f7d2097a365d32e79ab32e160012
Val17111997 <valentin@checks-studio.com>
2025-03-25 15:05:07 +0100

    Create auth.js

---
+++ pages/api/auth.js
@@ -0,0 +1,13 @@
+// pages/api/auth.js
+export default function handler(req, res) {
+  const shop = req.query.shop;
+
+  if (!shop) {
+    return res.status(400).send('Paramètre "shop" manquant');
+  }
+
+  const redirectUri = `${process.env.HOST}/api/callback`;
+  const installUrl = `https://${shop}/admin/oauth/authorize?client_id=${process.env.SHOPIFY_API_KEY}&scope=${process.env.SHOPIFY_SCOPES}&redirect_uri=${redirectUri}`;
+
+  res.redirect(installUrl);
+}
