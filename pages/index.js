a937491070216b2f4c8215a03b2a1f8aaddc0955
Valentin Cluzeau <valentincluzeau@MacBook-Air-de-Valentin.local>
2025-03-25 11:29:01 +0100

    Clean new version

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>EcomCore est en ligne 🚀</h1>
+      <p>Bienvenue dans ton app Shopify custom !</p>
+    </div>
+  );
+}
