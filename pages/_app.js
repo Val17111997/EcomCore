1be11acd0c7db1be081468b07a88de3cd96aa3a1
Val17111997 <valentin@checks-studio.com>
2025-03-27 09:20:59 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import "@/styles/globals.css";
+import { AppProvider } from '@shopify/polaris';
+import { Provider } from '@shopify/app-bridge-react';
+import '@shopify/polaris/build/esm/styles.css';
+import { useMemo } from 'react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  const config = useMemo(() => ({
+    apiKey: process.env.NEXT_PUBLIC_SHOPIFY_API_KEY,
+    host: new URLSearchParams(window.location.search).get("host"),
+    forceRedirect: true,
+  }), []);
+
+  return (
+    <AppProvider>
+      <Provider config={config}>
+        <Component {...pageProps} />
+      </Provider>
+    </AppProvider>
+  );
 }
+
+export default MyApp;
